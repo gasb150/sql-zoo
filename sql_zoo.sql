b50beca67428b139ea4e72f1e8b354efc6f80ca5
gasb150 <gasb_150@hotmail.com>
2021-01-05 15:32:04 -0500

    ADD tutorial 4 pt-1

--- sql_zoo.sql
+++ sql_zoo.sql
@@ -185,3 +185,52 @@ SELECT winner, subject
  WHERE yr=1984
  ORDER BY subject IN ('Physics', 'Chemistry'), subject, winner
  
+--SELECT within SELECT Tutorial
+
+--Bigger than Russia
+ SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
+--Richer than UK
+SELECT name
+FROM world
+WHERE gdp/population > 
+ (SELECT gdp/population
+ FROM world
+ WHERE name = 'United Kingdom') AND continent = 'Europe'
+--Neighbours of Argentina and Australia
+ SELECT name, continent
+FROM world
+WHERE continent IN
+(SELECT continent
+FROM world
+WHERE name IN ('Argentina','Australia'))
+ORDER BY name
+--Between Canada and Poland
+SELECT name, population
+FROM world
+WHERE population > 
+(SELECT population FROM world WHERE name = 'Canada') 
+AND population < 
+(SELECT population FROM world WHERE name = 'Poland')
+--Percentages of Germany
+SELECT name, CONCAT(ROUND(population/(SELECT population
+FROM world
+WHERE name = 'germany')*100),'%') AS percentage
+FROM world
+WHERE continent = 'Europe'
+-- Bigger than every country in Europe
+SELECT name
+FROM world
+WHERE gdp > ALL(SELECT gdp
+FROM world
+WHERE gdp>0
+AND continent = 'EUROPE')
+--Largest in each continent
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area>0)
+-- First country of each continent (alphabetically)

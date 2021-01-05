6b04b40dd88d9fec1e3e477c9c471c7fec6f2300
gasb150 <gasb_150@hotmail.com>
2021-01-05 18:18:14 -0500

    ADD tutorial 4

--- sql_zoo.sql
+++ sql_zoo.sql
@@ -234,3 +234,26 @@ SELECT continent, name, area FROM world x
         WHERE y.continent=x.continent
           AND area>0)
 -- First country of each continent (alphabetically)
+SELECT continent, name
+FROM world base
+WHERE name = 
+(SELECT name 
+FROM world compare
+WHERE base.continent = compare.continent
+LIMIT 1)
+-- Difficult Questions That Utilize Techniques Not Covered In Prior Sections
+SELECT name, continent, population FROM world
+WHERE continent IN
+  (SELECT continent FROM world x
+    WHERE
+      (SELECT MAX(population) FROM world y
+        WHERE x.continent = y.continent) <= 25000000);
+
+--  countries have populations more than three times that of any of their neighbours
+SELECT name, continent
+FROM world x
+WHERE population > ALL
+ (SELECT MAX(population * 3) FROM world y
+   WHERE
+     x.continent = y.continent
+       AND x.name <> y.name);
